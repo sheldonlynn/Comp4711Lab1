be32b7f31b20b35d1b26d0cefe704e2953e71786
Sheldon Lynn <sheldonlynn@outlook.com>
2018-01-14 17:07:07 -0800

    student class

--- student.php
+++ student.php
@@ -5,3 +5,20 @@
  * Date: 2018-01-14
  * Time: 5:05 PM
  */
+
+class Student {
+    function __construct() {
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+
+    function add_email($which,$address) {
+        $this->emails[$which] = $address;
+    }
+
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
+}
